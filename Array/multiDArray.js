4d267667a3d339d78a47923e1f637e4a14b105cf
vi5halsingh <vishalsinghlohdi16@gmail.com>
2026-01-31 00:16:55 +0530

    new Mulit D array and merge string

---
+++ Array/multiDArray.js
@@ -0,0 +1,13 @@
+
+const arr = [
+    [1 , 2 , 3],
+    [4 , 5 , 6],
+    [7 , 8 , 9]
+]
+
+for(let i = 0 ; i < arr.length ; i++){
+    for(let j = 0 ; j < arr[i].length ; j++){
+        process.stdout.write(`${arr[i][j]}`);
+    }
+    console.log();
+}
